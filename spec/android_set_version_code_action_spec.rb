09d17467a791ea936efa9199f49a096f906bed49
Salvatore Testa <sal@withpersona.com>
2022-01-25 14:32:29 -0800

    Support build.gradle.kts files
    - remove dependency of `Helper.test?`
    - test in a `./app/` folder instead of `./tmp/`
    - dynamically check for presence of Groovy, than Kotlin build  file

--- spec/android_set_version_code_action_spec.rb
+++ spec/android_set_version_code_action_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Fastlane::Actions::AndroidSetVersionCodeAction do
-  describe "Set Version Code" do
+  describe "Set Version Code (Groovy)" do
     before do
-      copy_project_files_fixture
+      copy_groovy_project_files_fixture
     end
 
     it "should increment Version Code and return its new value" do
@@ -18,6 +18,44 @@ describe Fastlane::Actions::AndroidSetVersionCodeAction do
         android_set_version_code
       end').runner.execute(:test)
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::ANDROID_NEW_VERSION_CODE]).to eq(18)
+    end
+
+    it "should set specific Version Code and return its new value" do
+      result = Fastlane::FastFile.new.parse('lane :test do
+        android_set_version_code(version_code: 17)
+      end').runner.execute(:test)
+      expect(result).to eq(17)
+    end
+
+    it "should set specific Version Code to ANDROID_NEW_VERSION_CODE shared value" do
+      Fastlane::FastFile.new.parse('lane :test do
+        android_set_version_code(version_code: 17)
+      end').runner.execute(:test)
+      expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::ANDROID_NEW_VERSION_CODE]).to eq(17)
+    end
+
+    after do
+      remove_project_files_fixture
+    end
+  end
+
+  describe "Set Version Code (Kotlin)" do
+    before do
+      copy_kotlin_project_files_fixture
+    end
+
+    it "should increment Version Code and return its new value" do
+      result = Fastlane::FastFile.new.parse('lane :test do
+        android_set_version_code
+      end').runner.execute(:test)
+      expect(result).to eq(124)
+    end
+
+    it "should set incremented Version Code to ANDROID_NEW_VERSION_CODE shared value" do
+      Fastlane::FastFile.new.parse('lane :test do
+        android_set_version_code
+      end').runner.execute(:test)
+      expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::ANDROID_NEW_VERSION_CODE]).to eq(124)
     end
 
     it "should set specific Version Code and return its new value" do
